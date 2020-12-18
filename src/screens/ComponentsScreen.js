8017df9b68a7d91ac84b501a22d30b5f9b0d60e0
lanibon <nguyenlan13@gmail.com>
2020-12-17 19:23:01 -0800

    initial commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const ComponentsScreen = () => {
+    return <Text>Components Screen</Text>
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
+    }
+})
+
+
+export default ComponentsScreen
